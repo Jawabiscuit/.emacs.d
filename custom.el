9523a2504a7b2ce79452bcbb6c20ecc6413de9a2
Jonas Avrin <jawabiscuit@users.noreply.github.com>
2018-09-17 22:24:29 -0400

    Init emacs with init.el instead of .emacs

---
+++ custom.el
@@ -0,0 +1,32 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-save-default nil)
+ '(auto-save-interval 0)
+ '(auto-save-list-file-prefix nil)
+ '(auto-save-timeout 0)
+ '(auto-show-mode t t)
+ '(custom-enabled-themes (quote (tsdh-dark)))
+ '(delete-auto-save-files nil)
+ '(delete-old-versions (quote other))
+ '(imenu-auto-rescan t)
+ '(imenu-auto-rescan-maxout 500000)
+ '(kept-new-versions 5)
+ '(kept-old-versions 5)
+ '(make-backup-file-name-function (quote ignore))
+ '(make-backup-files nil)
+ '(mouse-wheel-follow-mouse nil)
+ '(mouse-wheel-progressive-speed nil)
+ '(mouse-wheel-scroll-amount (quote (15)))
+ '(package-selected-packages
+   (quote
+    (smartparens restclient paredit magit-gitflow magit multiple-cursors markdown-mode+ markdown-mode minimap)))
+ '(version-control nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ init.el
@@ -0,0 +1,45 @@
+;; Turn off mouse interface early in startup to avoid momentary display
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+(package-initialize)
+
+;; No splash screen please ... jeez
+(setq inhibit-startup-message t)
+
+;; Setup load path
+(setq handmade-dir "W:/handmade/")
+(setq settings-dir
+      (concat handmade-dir
+	      (convert-standard-filename ".emacs.d/settings")))
+(add-to-list 'load-path settings-dir)
+
+;; Setup packages
+(require 'setup-package)
+
+;; Keep emacs Custom-settings in separate file
+(setq custom-file
+      (concat handmade-dir
+	      (convert-standard-filename ".emacs.d/custom.el")))
+(load custom-file)
+
+;; Install extensions if they're missing
+(defun init--install-packages ()
+  (packages-install
+   '(
+     magit
+     magit-gitflow
+     markdown-mode
+     multiple-cursors
+     paredit
+     restclient
+     smartparens
+     )))
+
+(condition-case nil
+    (init--install-packages)
+  (error
+   (package-refresh-contents)
+   (init--install-packages)))
+
