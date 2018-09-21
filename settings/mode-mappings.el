5481a6c302445ead8e04c48e27411c57b3a62a42
Jonas Avrin <jawabiscuit@users.noreply.github.com>
2018-09-20 22:17:08 -0400

    Combine mode mappings and make bright red todos

--- settings/mode-mappings.el
+++ settings/mode-mappings.el
@@ -1,25 +1,49 @@
-; Builtin
+;; Bright-red TODOs
+(setq fixme-modes '(c++-mode c-mode emacs-lisp-mode))
+ (make-face 'font-lock-fixme-face)
+ (make-face 'font-lock-study-face)
+ (make-face 'font-lock-important-face)
+ (make-face 'font-lock-note-face)
+ (mapc (lambda (mode)
+	 (font-lock-add-keywords
+	  mode
+	  '(("\\<\\(TODO\\)" 1 'font-lock-fixme-face t)
+	    ("\\<\\(STUDY\\)" 1 'font-lock-study-face t)
+	    ("\\<\\(IMPORTANT\\)" 1 'font-lock-important-face t)
+            ("\\<\\(NOTE\\)" 1 'font-lock-note-face t))))
+	fixme-modes)
+ (modify-face 'font-lock-fixme-face "Red" nil nil t nil t nil nil)
+ (modify-face 'font-lock-study-face "Yellow" nil nil t nil t nil nil)
+ (modify-face 'font-lock-important-face "Yellow" nil nil t nil t nil nil)
+ (modify-face 'font-lock-note-face "Dark Green" nil nil t nil t nil nil)
 
-;; HTML
-(add-to-list 'auto-mode-alist '("\\.html\\'" . crappy-jsp-mode))
-(add-to-list 'auto-mode-alist '("\\.tag$" . html-mode))
-(add-to-list 'auto-mode-alist '("\\.vm$" . html-mode))
-(add-to-list 'auto-mode-alist '("\\.ejs$" . html-mode))
-
-;; CSS
-(add-to-list 'auto-mode-alist '("\\.scss$" . css-mode))
-
-; Packages
-
-;; Markdown
-(autoload 'markdown-mode "markdown-mode")
-(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
-
-;; org-mode
-(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-
-;; Restclient
-(add-to-list 'auto-mode-alist '("\\.restclient$" . restclient-mode))
+;; Accepted file extensions and their appropriate modes
+(setq auto-mode-alist
+      (append
+       '(("\\.html\\'"     . crappy-jsp-mode)
+	 ("\\.tag$"        . html-mode)
+	 ("\\.vm$"         . html-mode)
+	 ("\\.ejs$"        . html-mode)
+	 ("\\.scss$"       . css-mode)
+	 ("\\.md$"         . markdown-mode)
+	 ("\\.markdown$"   . markdown-mode)
+	 ("\\.org$"        . org-mode)
+	 ("\\.cpp$"        . c++-mode)
+         ("\\.hin$"        . c++-mode)
+         ("\\.cin$"        . c++-mode)
+         ("\\.inl$"        . c++-mode)
+         ("\\.rdc$"        . c++-mode)
+         ("\\.h$"          . c++-mode)
+         ("\\.c$"          . c++-mode)
+         ("\\.cc$"         . c++-mode)
+         ("\\.c8$"         . c++-mode)
+         ("\\.txt$"        . indented-text-mode)
+         ("\\.emacs$"      . emacs-lisp-mode)
+         ("\\.gen$"        . gen-mode)
+         ("\\.ms$"         . fundamental-mode)
+         ("\\.m$"          . objc-mode)
+         ("\\.mm$"         . objc-mode)
+	 ("\\.restclient$" . restclient-mode)
+         ) auto-mode-alist))
 
 (provide 'mode-mappings)
