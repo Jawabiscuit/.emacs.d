7c629efbc42214e5d0b0d983863d822276e145e0
Jonas Avrin <jawabiscuit@users.noreply.github.com>
2018-11-06 22:50:02 -0500

    Update shell script

--- runemacs.bat
+++ runemacs.bat
@@ -2,6 +2,7 @@
 
 call activate python27
 
-set HOME=C:\Users\%USERNAME%
-set PATH="C:\Users\Jonas\AppData\Local\Pandoc";"C:\Program Files\MiKTeX 2.9\miktex\bin\x64";%PATH%
-"C:\Program Files (x86)\emacs\bin\runemacs.exe"
+set HOME=%USERPROFILE%
+set PATH="C:\Users\Jonas\AppData\Local\Pandoc";"C:\Program Files\MiKTeX 2.9\miktex\bin\x64";"C:\Program Files (x86)\emacs\bin\";%PATH%
+
+rem runemacs
