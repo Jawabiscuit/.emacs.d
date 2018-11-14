efaa8086fb1d866bf4e97641a0b57d82cc48b9fd
Jonas Avrin <jawabiscuit@users.noreply.github.com>
2018-11-13 22:28:49 -0500

    Remove runemacs.bat

--- runemacs.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-call activate python27
-
-set HOME=%USERPROFILE%
-set PATH="C:\Users\Jonas\AppData\Local\Pandoc";"C:\Program Files\MiKTeX 2.9\miktex\bin\x64";"C:\Program Files (x86)\emacs\bin\";%PATH%
-
-rem runemacs
