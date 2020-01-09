229b3051ab6c51e4094371bdb91f48d6c82c4c6d
Jonas Avrin <jawabiscuit@users.noreply.github.com>
2020-01-08 21:00:07 -0500

    Disable apheleia-global-mode, black freezes emacs

--- settings/setup-linting.el
+++ settings/setup-linting.el
@@ -53,8 +53,9 @@
 ;; Code formatter
 (use-package apheleia
   :straight (apheleia :host github :repo "raxod502/apheleia")
-  :config
-  (apheleia-global-mode +1))
+  ;; :config
+  ;; (apheleia-global-mode +1)
+)
 
 (provide 'setup-linting)
 ;;; setup-linting.el ends here
