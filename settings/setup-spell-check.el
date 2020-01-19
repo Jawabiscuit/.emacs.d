d55a11a7ef19d9e801ffc7527b714d55f2e525ad
Jonas Avrin <jawabiscuit@users.noreply.github.com>
2020-01-19 15:38:29 +0000

    Ensure spell check works for Windows as well as Linux

--- settings/setup-spell-check.el
+++ settings/setup-spell-check.el
@@ -30,19 +30,38 @@
 ;;
 ;;; Code:
 
+;; (when (executable-find "hunspell")
+;;    (setq ispell-alternate-dictionary "/usr/share/hunspell/en_US.dic"
+;;          ispell-local-dictionary-alist
+;;          '(("english"
+;;             "[[:alpha:]]"
+;;             "[^[:alpha:]]"
+;;             "[']"
+;;             t
+;;             ("-d" "en_US" "-p" "/usr/share/hunspell")
+;;             nil
+;;             iso-8859-1))
+;;          ispell-dictionary "english"))
+
 (when (executable-find "hunspell")
+  (setenv "LANG" "en_US.UTF-8")
   (setq ispell-program-name "hunspell"
-        ispell-alternate-dictionary "/usr/share/hunspell/en_US.dic"
-        ispell-local-dictionary-alist
-        '(("english"
-           "[[:alpha:]]"
-           "[^[:alpha:]]"
-           "[']"
-           t
-           ("-d" "en_US" "-p" "/usr/share/hunspell")
-           nil
-           iso-8859-1))
-        ispell-dictionary "english"))
+        ispell-dictionary "en_US")
+  (when sys/wslp (setq ispell-alternate-dictionary "/usr/share/hunspell/en_US.dic"
+                       ispell-local-dictionary-alist
+                       '(("english"
+                          "[[:alpha:]]"
+                          "[^[:alpha:]]"
+                          "[']"
+                          t
+                          ("-d" "en_US" "-p" "/usr/share/hunspell")
+                          nil
+                          iso-8859-1))
+                       ispell-dictionary "english"))
+  (when sys/win32p (setq ispell-program-name "hunspell"
+                         ispell-complete-word-dict
+                         (concat (expand-file-name user-emacs-directory)
+                                 "hunspell/share/hunspell/en_US.dic"))))
 
 (use-package flyspell
   :when (executable-find "hunspell")
