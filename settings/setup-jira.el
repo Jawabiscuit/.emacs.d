b1063da7fe730a1761e07292e17c71ffb420e6bf
Jonas Avrin <jawabiscuit@users.noreply.github.com>
2021-02-03 21:51:57 -0500

    Jira configuration

--- settings/setup-jira.el
+++ settings/setup-jira.el
@@ -1,80 +1,64 @@
-;;; setup-jira --- Jira integration settings -*- lexical-binding: t -*-
-
-;; Author: Jonas Avrin
-;; URL: https://www.github.com/jawabiscuit
-;; Package-Requires: (`')
-
-;; This file is not part of GNU Emacs
-;;
-;; This file is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-;;
-;; For a full copy of the GNU General Public License
-;; see <http://www.gnu.org/licenses/>.
-;;
-
+;; -*- lexical-binding: t -*-
+;;; `setup-jira.el' --- Summary: Jira settings
 ;;; Commentary:
-;;
-;;
-
 ;;; Code:
+(jawa/require 'system-const)
+
+(if sys/bss-hostname-p (setq jira-url "https://jira.blueskystudios.com"
+                             jira-login-name "jonasavrin"
+                             jira-projects-list '("CHARDEV", "MEDIA"))
+  (setq jira-url ""
+        jira-login-name ""
+        jira-projects-list '()))
 
 (use-package org-jira
   :straight (org-jira
              :type git
              :host github
              :repo "ahungry/org-jira")
-  :config (setq jiralib-url "https://jira.blueskystudios.com"))
+  :config (setq jiralib-url jira-url))
 
+(if sys/bss-hostname-p
+    (use-package ejira
+      :straight (ejira :type git :host github :repo "nyyManni/ejira")
+      :init
+      (setq jiralib2-url              jira-url
+            jiralib2-auth             'basic
+            jiralib2-user-login-name  jira-login-name
+            jiralib2-token
 
-(use-package ejira
-  :disabled t
-  :straight (ejira :type git :host github :repo "nyyManni/ejira")
-  :init
-  (setq jiralib2-url              "https://jira.blueskystudios.com"
-        jiralib2-auth             'basic
-        jiralib2-user-login-name  "jonasavrin"
-        jiralib2-token
+            ejira-org-directory       "~/org/jira"
+            ejira-projects            jira-projects-list
 
-        ejira-org-directory       "~/org/jira"
-        ejira-projects            '("CHARDEV")
+            ejira-priorities-alist    '(("Highest" . ?A)
+                                        ("High"    . ?B)
+                                        ("Medium"  . ?C)
+                                        ("Low"     . ?D)
+                                        ("Lowest"  . ?E))
+            ejira-todo-states-alist   '(("To Do"       . 1)
+                                        ("In Progress" . 2)
+                                        ("Done"        . 3)))
+      :config
+      ;; Tries to auto-set custom fields by looking into /editmeta
+      ;; of an issue and an epic.
+      (add-hook 'jiralib2-post-login-hook #'ejira-guess-epic-sprint-fields)
 
-        ejira-priorities-alist    '(("Highest" . ?A)
-                                    ("High"    . ?B)
-                                    ("Medium"  . ?C)
-                                    ("Low"     . ?D)
-                                    ("Lowest"  . ?E))
-        ejira-todo-states-alist   '(("To Do"       . 1)
-                                    ("In Progress" . 2)
-                                    ("Done"        . 3)))
-  :config
-  ;; Tries to auto-set custom fields by looking into /editmeta
-  ;; of an issue and an epic.
-  (add-hook 'jiralib2-post-login-hook #'ejira-guess-epic-sprint-fields)
+      ;; They can also be set manually if autoconfigure is not used.
+      ;; (setq ejira-sprint-field       'customfield_10001
+      ;;       ejira-epic-field         'customfield_10002
+      ;;       ejira-epic-summary-field 'customfield_10004)
 
-  ;; They can also be set manually if autoconfigure is not used.
-  ;; (setq ejira-sprint-field       'customfield_10001
-  ;;       ejira-epic-field         'customfield_10002
-  ;;       ejira-epic-summary-field 'customfield_10004)
+      (require 'ejira-agenda)
 
-  (require 'ejira-agenda)
+      ;; Make the issues visisble in your agenda by adding `ejira-org-directory'
+      ;; into your `org-agenda-files'.
+      (add-to-list 'org-agenda-files ejira-org-directory)
 
-  ;; Make the issues visisble in your agenda by adding `ejira-org-directory'
-  ;; into your `org-agenda-files'.
-  (add-to-list 'org-agenda-files ejira-org-directory)
-
-  ;; Add an agenda view to browse the issues that
-  (org-add-agenda-custom-command
-   '("j" "My JIRA issues"
-     ((ejira-jql "resolution = unresolved and assignee = currentUser()"
-                 ((org-agenda-overriding-header "Assigned to me")))))))
+      ;; Add an agenda view to browse the issues that
+      (org-add-agenda-custom-command
+       '("j" "My JIRA issues"
+         ((ejira-jql "resolution = unresolved and assignee = currentUser()"
+                     ((org-agenda-overriding-header "Assigned to me"))))))))
 
 (provide 'setup-jira)
 ;;; setup-jira.el ends here
