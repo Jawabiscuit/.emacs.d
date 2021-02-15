6c5c6a2a55bd803ed9c1ae10ba3315abfedbde2b
Jonas Avrin <jawabiscuit@users.noreply.github.com>
2021-02-14 19:07:34 -0500

    More configuration around persp mode and projectile mode

--- settings/setup-persp.el
+++ settings/setup-persp.el
@@ -14,39 +14,41 @@
   )
 
 (use-package persp-mode
+  :straight (persp-mode :type git :flavor melpa :host github :repo "Bad-ptr/persp-mode.el")
   :diminish
   :defines (recentf-exclude ivy-ignore-buffers ivy-sort-functions-alist)
   :commands (get-current-persp persp-contain-buffer-p)
-  :hook (;; Interfering with dashbord
+  :hook (;; Has been known to interfere with dashbord
          (after-init . persp-mode)
          (window-setup . toggle-frame-maximized))
-  :init (setq persp-keymap-prefix (kbd "C-x p")
-              persp-nil-name "default"
-              persp-set-last-persp-for-new-frames nil
-              persp-kill-foreign-buffer-behaviour 'kill
-              persp-auto-resume-time 1.0
-              persp-common-buffer-filter-functions
-              (list #'(lambda (b)
-                        "Ignore temporary buffers."
-                        (let ((bname (file-name-nondirectory (buffer-name b))))
-                          (or (string-prefix-p " " bname)
-                              (and (string-prefix-p "*" bname)
-                                   (not (string-equal "*scratch*" bname)))
-                              (string-suffix-p ".elc" bname)
-                              (string-suffix-p ".gz" bname)
-                              (string-suffix-p ".zip" bname)
-                              (string-prefix-p "magit" bname)
-                              (string-prefix-p "Pfuture-Callback" bname)
-                              (string-match-p ".elfeed" bname)
-                              (eq (buffer-local-value 'major-mode b) 'erc-mode)
-                              (eq (buffer-local-value 'major-mode b) 'rcirc-mode)
-                              (eq (buffer-local-value 'major-mode b) 'nov-mode)
-                              (eq (buffer-local-value 'major-mode b) 'vterm-mode))))))
+  :init
+  (setq persp-keymap-prefix (kbd "C-x p")
+        persp-nil-name "default"
+        persp-set-last-persp-for-new-frames nil
+        persp-kill-foreign-buffer-behaviour 'kill
+        persp-auto-resume-time 1.0
+        persp-common-buffer-filter-functions
+        (list
+         #'(lambda (b)
+             "Ignore temporary buffers."
+             (let ((bname (file-name-nondirectory (buffer-name b))))
+               (or (string-prefix-p " " bname)
+                   (and (string-prefix-p "*" bname)
+                        (not (string-equal "*scratch*" bname)))
+                   (string-suffix-p ".elc" bname)
+                   (string-suffix-p ".gz" bname)
+                   (string-suffix-p ".zip" bname)
+                   (string-prefix-p "magit" bname)
+                   (string-prefix-p "Pfuture-Callback" bname)
+                   (string-match-p ".elfeed" bname)
+                   (eq (buffer-local-value 'major-mode b) 'erc-mode)
+                   (eq (buffer-local-value 'major-mode b) 'rcirc-mode)
+                   (eq (buffer-local-value 'major-mode b) 'nov-mode)
+                   (eq (buffer-local-value 'major-mode b) 'vterm-mode))))))
   :config
   (defun jawa/persp-mode-ibuffer-filter-groups-all ()
     "Shows groups for all perspectives. But can't show same buffer in multiple groups."
     (with-eval-after-load "ibuffer"
-      
       (require 'ibuf-ext)
       
       (define-ibuffer-filter persp-all
@@ -89,7 +91,6 @@
   (defun jawa/persp-mode-ibuffer-filter-groups-current ()
 
     (with-eval-after-load "ibuffer"
-
       (require 'ibuf-ext)
 
       (define-ibuffer-filter persp-current
@@ -113,7 +114,7 @@
                     (persp-add-ibuffer-group)
                     (ibuffer-switch-to-saved-filter-groups "persp-mode")))))
 
-  ;; Kill the buffer if it removed from every(or non weak) perspective
+  ;; Kill the buffer if it removed from every (or non weak) perspective
   (setq persp-autokill-buffer-on-remove 'kill-weak)
 
   ;; Don't save persp configs in `recentf'
@@ -121,13 +122,22 @@
 
   ;; Ivy Integration - https://gist.github.com/Bad-ptr/1aca1ec54c3bdb2ee80996eb2b68ad2d#file-persp-ivy-el
   (with-eval-after-load 'ivy
-    (add-to-list 'ivy-ignore-buffers
-                 #'(lambda (b)
-                     (when persp-mode
-                       (let ((persp (get-current-persp)))
-                         (if persp
-                             (not (persp-contain-buffer-p b persp))
-                           nil))))))
+    (add-to-list
+     'ivy-ignore-buffers
+     #'(lambda (b)
+         (when persp-mode
+           (let ((persp (get-current-persp)))
+             (if persp
+                 (not (persp-contain-buffer-p b persp))
+               nil)))))
+    (setq ivy-sort-functions-alist
+          (append ivy-sort-functions-alist
+                  '((persp-kill-buffer   . nil)
+                    (persp-remove-buffer . nil)
+                    (persp-add-buffer    . nil)
+                    (persp-switch        . nil)
+                    (persp-window-switch . nil)
+                    (persp-frame-switch  . nil)))))
 
   ;; To quickly launch emacs to edit a single file without loading perspectives
   ;; Then in a bash terminal: emacs -persp-q <file>
@@ -136,16 +146,135 @@
   ;; emacs -persp-q $@;
   ;; Then:
   ;; export EDITOR="editor.sh"
-  (add-to-list 'command-switch-alist
-               (cons "persp-q"
-                     #'(lambda (p)
-                         (setq persp-auto-resume-time -1
-                               persp-auto-save-opt 0))))
-  :bind
-  (("C-c h P" . hydra-persp/body)))
+  (add-to-list
+   'command-switch-alist
+   (cons "persp-q"
+         #'(lambda (p)
+             (setq persp-auto-resume-time -1
+                   persp-auto-save-opt 0))))
 
-;; Projectile integration
+  ;; Python inferior buffer save load
+  (add-to-list 'persp-save-buffer-functions
+               #'(lambda (b)
+                   (when (eq 'inferior-python-mode (buffer-local-value 'major-mode b))
+                     `(def-inferior-python-buffer ,(buffer-name b)
+                        ,(let ((process (get-buffer-process b)))
+                           (if process
+                               (progn
+                                 (python-shell-send-string "import os" process)
+                                 (python-shell-send-string-no-output "os.getcwd()" process))
+                             (concat "'" (buffer-local-value 'default-directory b) "'")))))))
+
+  (add-to-list 'persp-load-buffer-functions
+               #'(lambda (savelist)
+                   (when (eq (car savelist) 'def-inferior-python-buffer)
+                     (destructuring-bind (bname dir) (cdr savelist)
+                       (run-python nil nil nil)
+                       (with-current-buffer (python-shell-get-buffer)
+                         (rename-buffer bname)
+                         (cd dir)
+                         (python-shell-send-string "import os")
+                         (python-shell-send-string (format "os.chdir(%s)" dir))
+                         (current-buffer))))))
+
+  ;; Persp projectile auto persp
+  (defvar persp-mode-projectile-bridge-before-switch-selected-window-buffer nil)
+
+  ;; (setq persp-add-buffer-on-find-file 'if-not-autopersp)
+  ;; https://gist.github.com/Bad-ptr/1aca1ec54c3bdb2ee80996eb2b68ad2d#file-persp-projectile-auto-persp-el
+  (persp-def-auto-persp
+   "projectile"
+   :parameters '((dont-save-to-file . t)
+                 (persp-mode-projectile-bridge . t))
+   :hooks '(projectile-before-switch-project-hook
+            projectile-after-switch-project-hook
+            projectile-find-file-hook
+            find-file-hook)
+   :dyn-env '((after-switch-to-buffer-adv-suspend t))
+   :switch 'frame
+   :predicate
+   #'(lambda (buffer &optional state)
+       (if (eq 'projectile-before-switch-project-hook
+               (alist-get 'hook state))
+           state
+         (and
+          projectile-mode
+          (buffer-live-p buffer)
+          (buffer-file-name buffer)
+          ;; (not git-commit-mode)
+          (projectile-project-p)
+          (or state t))))
+   :get-name
+   #'(lambda (state)
+       (if (eq 'projectile-before-switch-project-hook
+               (alist-get 'hook state))
+           state
+         (push (cons 'persp-name
+                     (concat "p) "
+                             (with-current-buffer (alist-get 'buffer state)
+                               (projectile-project-name))))
+               state)
+         state))
+   :on-match
+   #'(lambda (state)
+       (let ((hook (alist-get 'hook state))
+             (persp (alist-get 'persp state))
+             (buffer (alist-get 'buffer state)))
+         (case hook
+           (projectile-before-switch-project-hook
+            (let ((win (if (minibuffer-window-active-p (selected-window))
+                           (minibuffer-selected-window)
+                         (selected-window))))
+              (when (window-live-p win)
+                (setq persp-mode-projectile-bridge-before-switch-selected-window-buffer
+                      (window-buffer win)))))
+
+           (projectile-after-switch-project-hook
+            (when (buffer-live-p
+                   persp-mode-projectile-bridge-before-switch-selected-window-buffer)
+              (let ((win (selected-window)))
+                (unless (eq (window-buffer win)
+                            persp-mode-projectile-bridge-before-switch-selected-window-buffer)
+                  (set-window-buffer
+                   win persp-mode-projectile-bridge-before-switch-selected-window-buffer)))))
+
+           (find-file-hook
+            (setcdr (assq :switch state) nil)))
+         (if (case hook
+               (projectile-before-switch-project-hook nil)
+               (t t))
+             (persp--auto-persp-default-on-match state)
+           (setcdr (assq :after-match state) nil)))
+       state)
+   :after-match
+   #'(lambda (state)
+       (when (eq 'find-file-hook (alist-get 'hook state))
+         (run-at-time 0.5 nil
+                      #'(lambda (buf persp)
+                          (when (and (eq persp (get-current-persp))
+                                     (not (eq buf (window-buffer (selected-window)))))
+                            ;; (switch-to-buffer buf)
+                            (persp-add-buffer buf persp t nil)))
+                      (alist-get 'buffer state)
+                      (get-current-persp)))
+       (persp--auto-persp-default-after-match state)))
+
+    ;; (add-hook 'persp-after-load-state-functions
+    ;;           #'(lambda (&rest args) (persp-auto-persps-pickup-buffers)) t)
+
+  :bind
+  (("C-c h P" . hydra-persp/body)
+   ;; Global overrides
+   ("C-x b" . persp-switch-to-buffer)
+   ("C-x k" . persp-kill-buffer)))
+
+
+;; Persp mode projectile integration
 (use-package persp-mode-projectile-bridge
+  :straight
+  (persp-mode-projectile-bridge
+   :type git :flavor melpa
+   :host github :repo "Bad-ptr/persp-mode-projectile-bridge.el")
   :after (:all (persp-mode projectile-mode))
   :functions (persp-get-by-name
               persp-add-new
@@ -158,8 +287,7 @@
              projectile-project-buffers)
   :hook ((persp-mode . projectile-mode)
          (persp-mode . persp-mode-projectile-bridge-mode)
-         (persp-mode-projectile-bridge-mode
-          .
+         (persp-mode-projectile-bridge-mode .
           (lambda ()
             (if persp-mode-projectile-bridge-mode
                 (persp-mode-projectile-bridge-find-perspectives-for-all-buffers)
